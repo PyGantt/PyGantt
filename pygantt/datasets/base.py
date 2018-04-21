bb9cceb6bbeab45e54fbae81727ce16d9215c06c
okomarov <oleg.komarov11@gmail.com>
2018-04-21 18:36:17 +0100

    add new dataset load methods

--- pygantt/datasets/base.py
+++ pygantt/datasets/base.py
@@ -7,3 +7,17 @@ def load_concert():
     module_path = os.path.dirname(__file__)
 
     return pd.read_csv(os.path.join(module_path, 'data','concert.csv'), parse_dates=['Start','Finish'])
+
+def load_equities():
+    """Loads equities positions data.
+    """
+    module_path = os.path.dirname(__file__)
+
+    return pd.read_csv(os.path.join(module_path, 'data','equities.csv'), parse_dates=['Start','Finish'])
+
+def load_equities_ptf():
+    """Loads equities portfolio price series.
+    """
+    module_path = os.path.dirname(__file__)
+
+    return pd.read_csv(os.path.join(module_path, 'data','equities_ptf.csv'), parse_dates=['Date'])
